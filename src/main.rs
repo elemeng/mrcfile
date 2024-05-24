8aaed7cf4b97584fa2de49ae7758ee16431f1e97
elemeng <tjumxm@outlook.com>
2024-05-24 19:04:40 +0800

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use std::fs::File;
-use mrcfile::{image_processing::contrast::adjust_contrast};
+//use mrcfile::{image_processing::contrast::adjust_contrast};
 use mrcfile::header::parser::MrcHeader;
 
 #[derive(Parser)]
@@ -10,39 +10,10 @@ struct Cli {
     file_path: String,
 }
 
-
-
 fn main() {
-    let matches = App::new("MRC Toolkit")
-        .version("1.0")
-        .about("Tools for processing MRC files")
-        .subcommand(
-            SubCommand::with_name("info")
-                .about("Displays header information")
-                .arg(Arg::with_name("FILE").help("Input MRC file").required(true)),
-        )
-        .subcommand(
-            SubCommand::with_name("adjust-contrast")
-                .about("Adjusts image contrast")
-                .arg(Arg::with_name("FILE").help("Input MRC file").required(true))
-                .arg(Arg::with_name("CONTRAST").help("Contrast value").required(true)),
-        )
-        .get_matches();
-
-    if let Some(matches) = matches.subcommand_matches("info") {
-        let filename = matches.value_of("FILE").unwrap();
-        let mut file = File::open(filename).expect("Failed to open file");
-        let header = MrcHeader::from_reader(&mut file).expect("Failed to read header");
-        println!("{:?}", header);
-    }
-
-    if let Some(matches) = matches.subcommand_matches("adjust-contrast") {
-        let filename = matches.value_of("FILE").unwrap();
-        let contrast: f32 = matches.value_of("CONTRAST").unwrap().parse().expect("Invalid contrast value");
-        let mut file = File::open(filename).expect("Failed to open file");
-        let mut buffer = Vec::new();
-        file.read_to_end(&mut buffer).expect("Failed to read file");
-        adjust_contrast(&mut buffer, contrast);
-        // Save the adjusted image back or display it
+    let args = Args::parse();
+    match mrc::parser::parse_header(&args.file) {
+        Ok(header) => println!("{:?}", header),
+        Err(e) => eprintln!("Error parsing file: {}", e),
     }
 }
